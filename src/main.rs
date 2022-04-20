62145b8e49a68c13e8148938dfbb5699b3755ead
Your Name <you@example.com>
2022-04-20 22:37:58 +0100

    add file switching

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ mod style;
 use std::{
     fs::File,
     io::{Read, Write},
-    path::{PathBuf},
+    path::PathBuf,
 };
 
-use chrono::{DateTime, Datelike, Local};
+use chrono::{Date, DateTime, Datelike, Local, LocalResult, NaiveDate, TimeZone};
 use directories::ProjectDirs;
 use eframe::{
     egui::{self, TextEdit},
@@ -14,17 +14,22 @@ use eframe::{
 };
 use walkdir::WalkDir;
 
+#[derive(Copy, Clone, Debug)]
 enum BufferId {
-    Date(DateTime<Local>),
+    Date(Date<Local>),
 }
 
 impl Default for BufferId {
     fn default() -> Self {
-        Self::Date(Local::now())
+        Self::Date(Local::now().date())
     }
 }
 
 impl BufferId {
+    fn today() -> Self {
+        BufferId::default()
+    }
+
     fn filepath(&self) -> PathBuf {
         match self {
             Self::Date(dt) => {
@@ -42,16 +47,59 @@ impl BufferId {
 struct MyEguiApp {
     buffer_id: BufferId,
     buffer: String,
+    available_buffers: Vec<BufferId>,
 }
 
 impl MyEguiApp {
+    pub fn load() -> Self {
+        let mut s = Self::default();
+        let _ = s.load_buffer();
+        s.update_available_buffers();
+        s
+    }
 
     fn root_dir(&self) -> PathBuf {
         if let Some(project_dirs) = ProjectDirs::from("com", "marschium", "notez") {
             project_dirs.data_dir().into()
-        }
-        else {
+        } else {
             ".".into()
+        }
+    }
+
+    fn update_available_buffers(&mut self) {
+        self.available_buffers.clear();
+        for entry in WalkDir::new(self.root_dir())
+            .into_iter()
+            .filter_map(|e| e.ok())
+        {
+            if entry.path().is_file() {
+                let mut components = entry.path().components().into_iter().rev();
+                let day = components
+                    .next()
+                    .and_then(|x| x.as_os_str().to_str())
+                    .and_then(|x| x.parse::<u32>().ok());
+                let month = components
+                    .next()
+                    .and_then(|x| x.as_os_str().to_str())
+                    .and_then(|x| x.parse::<u32>().ok());
+                let year = components
+                    .next()
+                    .and_then(|x| x.as_os_str().to_str())
+                    .and_then(|x| x.parse::<i32>().ok());
+
+                match (year, month, day) {
+                    (Some(year), Some(month), Some(day)) => {
+                        let d = Local.ymd_opt(year, month, day);
+                        match d {
+                            LocalResult::Single(d) => {
+                                self.available_buffers.push(BufferId::Date(d));
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
         }
     }
 
@@ -80,6 +128,12 @@ impl MyEguiApp {
             Err(e) => Err(e),
         }
     }
+
+    fn swap_to_buffer(&mut self, id: &BufferId) {
+        let _ = self.save_buffer();
+        self.buffer_id = *id;
+        let _ = self.load_buffer();
+    }
 }
 
 impl epi::App for MyEguiApp {
@@ -88,21 +142,27 @@ impl epi::App for MyEguiApp {
     }
 
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
-        egui::SidePanel::left("Hello World").show(ctx, |ui| {
-            // TODO cache and only walk when something changes
-            for entry in WalkDir::new(self.root_dir()).into_iter().filter_map(|e| e.ok()) {
-                if entry.path().is_file() {
-                    ui.label(entry.path().to_str().unwrap_or("???"));
+        egui::SidePanel::left("buffers").show(ctx, |ui| {
+            for id in self.available_buffers.clone() {
+                if ui.button(id.filepath().to_str().unwrap()).clicked() {
+                    self.swap_to_buffer(&id)
                 }
             }
         });
         egui::CentralPanel::default().show(ctx, |ui| {
-            if ui.button("Save").clicked() {
-                self.save_buffer(); // TODO show error
-            }
-            if ui.button("Load").clicked() {
-                self.load_buffer(); // TODO show error
-            }
+            // TODO autosave
+            // TODO make these hotkeys
+            ui.horizontal(|ui| {
+                if ui.button("Save").clicked() {
+                    let _ = self.save_buffer(); // TODO show error
+                }
+                if ui.button("Load").clicked() {
+                    let _ = self.load_buffer(); // TODO show error
+                }
+                if ui.button("Today").clicked() {
+                    self.swap_to_buffer(&BufferId::today())
+                }
+            });
 
             let mut layouter = |ui: &egui::Ui, string: &str, wrap_width: f32| {
                 let mut layout_job: egui::text::LayoutJob = style::highlight(string);
@@ -118,7 +178,7 @@ impl epi::App for MyEguiApp {
 }
 
 fn main() {
-    let app = MyEguiApp::default();
+    let app = MyEguiApp::load();
     // TODO load the current date entry if exists. create new one if not
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(app), native_options);

c7c5911ccb88fb77408abfa06ca119c091622055
c <>
2022-04-20 20:53:32 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,18 @@
+mod style;
+
 use std::{
     fs::File,
     io::{Read, Write},
-    path::{Path, PathBuf},
+    path::{PathBuf},
 };
-
-pub struct LinesWithEndings<'a> {
-    input: &'a str,
-}
-
-impl<'a> LinesWithEndings<'a> {
-    pub fn from(input: &'a str) -> LinesWithEndings<'a> {
-        LinesWithEndings { input: input }
-    }
-}
-
-impl<'a> Iterator for LinesWithEndings<'a> {
-    type Item = &'a str;
-
-    #[inline]
-    fn next(&mut self) -> Option<&'a str> {
-        if self.input.is_empty() {
-            return None;
-        }
-        let split = self
-            .input
-            .find('\n')
-            .map(|i| i + 1)
-            .unwrap_or(self.input.len());
-        let (line, rest) = self.input.split_at(split);
-        self.input = rest;
-        Some(line)
-    }
-}
-
-enum Error {
-    IoError(std::io::Error),
-}
 
 use chrono::{DateTime, Datelike, Local};
+use directories::ProjectDirs;
 use eframe::{
-    egui::{self, TextEdit, TextFormat},
-    epaint::{
-        text::{LayoutJob, LayoutSection},
-        Color32, FontFamily, FontId,
-    },
+    egui::{self, TextEdit},
     epi,
 };
+use walkdir::WalkDir;
 
 enum BufferId {
     Date(DateTime<Local>),
@@ -78,8 +45,19 @@ struct MyEguiApp {
 }
 
 impl MyEguiApp {
+
+    fn root_dir(&self) -> PathBuf {
+        if let Some(project_dirs) = ProjectDirs::from("com", "marschium", "notez") {
+            project_dirs.data_dir().into()
+        }
+        else {
+            ".".into()
+        }
+    }
+
     fn save_buffer(&self) -> Result<(), std::io::Error> {
-        let path = self.buffer_id.filepath();
+        let mut path = self.root_dir();
+        path.push(self.buffer_id.filepath());
         std::fs::create_dir_all(path.parent().unwrap())?;
         match File::create(path) {
             Ok(mut f) => {
@@ -91,7 +69,9 @@ impl MyEguiApp {
     }
 
     fn load_buffer(&mut self) -> Result<(), std::io::Error> {
-        match File::open(self.buffer_id.filepath()) {
+        let mut path = self.root_dir();
+        path.push(self.buffer_id.filepath());
+        match File::open(path) {
             Ok(mut f) => {
                 self.buffer.clear();
                 f.read_to_string(&mut self.buffer)?;
@@ -102,45 +82,20 @@ impl MyEguiApp {
     }
 }
 
-fn as_byte_range(whole: &str, range: &str) -> std::ops::Range<usize> {
-    let whole_start = whole.as_ptr() as usize;
-    let range_start = range.as_ptr() as usize;
-    assert!(whole_start <= range_start);
-    assert!(range_start + range.len() <= whole_start + whole.len());
-    let offset = range_start - whole_start;
-    offset..(offset + range.len())
-}
-
-fn layout_test(text: &str) -> LayoutJob {
-    let mut job = LayoutJob::default();
-    job.text = text.into();
-    let mut color = Color32::WHITE;
-    for line in LinesWithEndings::from(text) {
-        job.sections.push(LayoutSection {
-            byte_range: as_byte_range(text, line),
-            leading_space: 0.0,
-            format: TextFormat {
-                font_id: FontId::new(14.0, FontFamily::Proportional),
-                color,
-                ..Default::default()
-            },
-        });
-
-        match color {
-            Color32::WHITE => color = Color32::RED,
-            _ => color = Color32::WHITE,
-        }
-    }
-
-    job
-}
-
 impl epi::App for MyEguiApp {
     fn name(&self) -> &str {
         "My egui App"
     }
 
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
+        egui::SidePanel::left("Hello World").show(ctx, |ui| {
+            // TODO cache and only walk when something changes
+            for entry in WalkDir::new(self.root_dir()).into_iter().filter_map(|e| e.ok()) {
+                if entry.path().is_file() {
+                    ui.label(entry.path().to_str().unwrap_or("???"));
+                }
+            }
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.button("Save").clicked() {
                 self.save_buffer(); // TODO show error
@@ -150,7 +105,7 @@ impl epi::App for MyEguiApp {
             }
 
             let mut layouter = |ui: &egui::Ui, string: &str, wrap_width: f32| {
-                let mut layout_job: egui::text::LayoutJob = layout_test(string);
+                let mut layout_job: egui::text::LayoutJob = style::highlight(string);
                 layout_job.wrap_width = wrap_width;
                 ui.fonts().layout_job(layout_job)
             };
---
+++ src/style.rs
@@ -0,0 +1,68 @@
+use eframe::{epaint::{text::{LayoutJob, LayoutSection}, Color32, FontFamily, FontId}, egui::TextFormat};
+
+pub struct LinesWithEndings<'a> {
+    input: &'a str,
+}
+
+impl<'a> LinesWithEndings<'a> {
+    pub fn from(input: &'a str) -> LinesWithEndings<'a> {
+        LinesWithEndings { input: input }
+    }
+}
+
+impl<'a> Iterator for LinesWithEndings<'a> {
+    type Item = &'a str;
+
+    #[inline]
+    fn next(&mut self) -> Option<&'a str> {
+        if self.input.is_empty() {
+            return None;
+        }
+        let split = self
+            .input
+            .find('\n')
+            .map(|i| i + 1)
+            .unwrap_or(self.input.len());
+        let (line, rest) = self.input.split_at(split);
+        self.input = rest;
+        Some(line)
+    }
+}
+
+fn as_byte_range(whole: &str, range: &str) -> std::ops::Range<usize> {
+    let whole_start = whole.as_ptr() as usize;
+    let range_start = range.as_ptr() as usize;
+    assert!(whole_start <= range_start);
+    assert!(range_start + range.len() <= whole_start + whole.len());
+    let offset = range_start - whole_start;
+    offset..(offset + range.len())
+}
+
+pub fn highlight(text: &str) -> LayoutJob {
+    // TODO nom here?
+    let mut job = LayoutJob::default();
+    job.text = text.into();
+    
+    for line in LinesWithEndings::from(text) {
+        let mut format = TextFormat {
+            font_id: FontId::new(14.0, FontFamily::Proportional),
+            color: Color32::DARK_GRAY,
+            ..Default::default()
+        };
+
+        let trimmed = line.trim();
+        if trimmed.starts_with("[/]") {
+            format.color = Color32::GREEN;
+        }
+        else if trimmed.starts_with("[x]") {
+            format.color = Color32::RED;
+        }
+        job.sections.push(LayoutSection {
+            byte_range: as_byte_range(text, line),
+            leading_space: 0.0,
+            format,
+        });
+    }
+
+    job
+}

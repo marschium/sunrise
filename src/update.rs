51799ff2ab325f390ce870e2456f8a3a51288f35
c <>
2022-08-06 09:36:58 +0100

    wip

--- src/update.rs
+++ src/update.rs
@@ -5,7 +5,7 @@ pub const GITHUB_VERSION: Option<&'static str> = option_env!("build_version");
 
 pub fn current_version() -> semver::Version {
     let parsed = match GITHUB_VERSION {
-        Some(v) => semver::Version::parse(v),
+        Some(v) => semver::Version::parse(&v[1..]), // remove leading 'v'
         None => semver::Version::parse(VERSION)
     };
     parsed.unwrap_or(semver::Version::new(1, 0, 0))
